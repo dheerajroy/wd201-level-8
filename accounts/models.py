6f95f47e617b1a96176453d4ff823479a2e32262
dheerajroy <dhiraajroy123@gmail.com>
2022-03-07 23:54:56 +0530

    updated filtering in periodic_task

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     report_time = models.TimeField(null=True)
+    last_report_sent = models.DateField(null=True)
 
     def __str__(self):
         return self.user.username
