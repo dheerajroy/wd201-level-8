f1946f7d159d521ae47e13a58a5c865e4fd428f6
dheerajroy <dhiraajroy123@gmail.com>
2022-03-10 20:22:42 +0530

    used lte and le to filter users with report_time

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -7,7 +7,7 @@ from accounts.models import CustomUser
 
 @periodic_task(run_every=timedelta(minutes=1))
 def every_minute():
-    users = CustomUser.objects.filter(report_time__lte=datetime.now().time, last_report_sent__lt=datetime.now().date)
+    users = CustomUser.objects.filter(report_time__lte=datetime.now().time(), last_report_sent__lt=datetime.now().date())
     for user in users:
         tasks_status = {STATUS_CHOICES[i][0]: Task.objects.filter(status=STATUS_CHOICES[i][0], user=user.user).count() for i in range(len(STATUS_CHOICES))}
         email_body = f'''Your Tasks Summary:
